448a1af5fe8a5c346447901502ba2611d2762b21
Planva <planvaofficial@gmail.com>
2025-09-06 20:05:37 +0800

    fix8

--- lib/db/drizzle.ts
+++ lib/db/drizzle.ts
@@ -1,10 +1,7 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
-import * as schema from './schema'
+// lib/db/drizzle.ts
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema';
 
-const url = process.env.DB_URL
-if (!url) throw new Error('Missing DB_URL')
-
-const sql = neon(url)
-export const db = drizzle(sql, { schema })
-export const getDb = () => db
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
